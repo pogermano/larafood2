3d0d68035b2784dc4db0d8f24ce669b72e3d212e
Pedro O Germano <pogermano@gmail.com>
2023-03-08 16:44:47 -0300

    implementação de auth e site com planos

--- routes/web.php
+++ routes/web.php
@@ -1,78 +1,110 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->namespace('App\Http\Controllers\Admin')->group(function () {
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-    /**
-     * Routes Plan details
-     */
-    Route::get('plans/{url}/details/create', 'DetailPlanController@create')->name('details.plan.create');
-    Route::delete('plans/{url}/details/{idDetail}', 'DetailPlanController@destroy')->name('details.plan.destroy');
-    Route::get('plans/{url}/details/{idDetail}', 'DetailPlanController@show')->name('details.plan.show');
-    Route::put('plans/{url}/details/{idDetail}', 'DetailPlanController@update')->name('details.plan.update');
-    Route::get('plans/{url}/details/{idDetail}/edit', 'DetailPlanController@edit')->name('details.plan.edit');
+Route::prefix('admin')
+    ->namespace('App\Http\Controllers\Admin')
+    ->middleware(['auth', 'verified'])
+    ->group(function () {
 
-
-    Route::get('plans/{url}/details', 'DetailPlanController@index')->name('details.plan.index');
-    Route::post('plans/{url}/details', 'DetailPlanController@store')->name('details.plan.store');
-
-
-
-
-    /**
-     * Routes Plans
-     */
-    Route::get('plans/create', 'PlanController@create')->name('plans.create');
-    Route::any('plans/search', 'PlanController@search')->name('plans.search');
-    Route::delete('plans/{id}', 'PlanController@delete')->name('plans.delete');
-
-    Route::post('plans', 'PlanController@store')->name('plans.store');
-    Route::get('plans', 'PlanController@index')->name('plans.index');
-
-    Route::get('plans/{url}', 'PlanController@show')->name('plans.show');
-    Route::put('plans/{url}', 'PlanController@update')->name('plans.update');
-    Route::get('plans/{url}/edit', 'PlanController@edit')->name('plans.edit');
+        /**
+         * Routes Plan details
+         */
+        Route::get('plans/{url}/details/create', 'DetailPlanController@create')->name('details.plan.create');
+        Route::delete('plans/{url}/details/{idDetail}', 'DetailPlanController@destroy')->name('details.plan.destroy');
+        Route::get('plans/{url}/details/{idDetail}', 'DetailPlanController@show')->name('details.plan.show');
+        Route::put('plans/{url}/details/{idDetail}', 'DetailPlanController@update')->name('details.plan.update');
+        Route::get('plans/{url}/details/{idDetail}/edit', 'DetailPlanController@edit')->name('details.plan.edit');
 
 
-    /**
-     * Routes Profiles
-     */
+        Route::get('plans/{url}/details', 'DetailPlanController@index')->name('details.plan.index');
+        Route::post('plans/{url}/details', 'DetailPlanController@store')->name('details.plan.store');
 
-    Route::any('profiles/search', 'ACL\ProfileController@search')->name('profiles.search');
-    Route::resource('profiles', 'ACL\ProfileController');
 
-       /**
-     * Plan x Profile
-     */
-    Route::get('plans/{id}/profile/{idProfile}/detach', 'ACL\PlanProfileController@detachProfilePlan')->name('plans.profile.detach');
-    Route::post('plans/{id}/profiles', 'ACL\PlanProfileController@attachProfilesPlan')->name('plans.profiles.attach');
-    Route::any('plans/{id}/profiles/create', 'ACL\PlanProfileController@profilesAvailable')->name('plans.profiles.available');
-    Route::get('plans/{id}/profiles', 'ACL\PlanProfileController@profiles')->name('plans.profiles');
-    Route::get('profiles/{id}/plans', 'ACL\PlanProfileController@plans')->name('profiles.plans');
-    /**
-     * Routes Permissions x Profiles
-     */
-    Route::get('profiles/{id}/permission/{idPermission}/detach', 'ACL\PermissionProfileController@detachPermissionProfile')->name('profiles.permission.detach');
-    Route::any('profiles/{id}/permissions/create', 'ACL\PermissionProfileController@permissionsAvailable')->name('profiles.permissions.available');
-    Route::post('profiles/{id}/permissions', 'ACL\PermissionProfileController@attachPermissionsProfile')->name('profiles.permissions.attach');
-    Route::get('profiles/{id}/permissions', 'ACL\PermissionProfileController@permissions')->name('profiles.permissions');
-    Route::get('permission/{id}/profiles', 'ACL\PermissionProfileController@profiles')->name('permission.profiles');
 
-    /**
-     * Routes Permissions
-     */
 
-    Route::any('permissions/search', 'ACL\PermissionController@search')->name('permissions.search');
-    Route::resource('permissions', 'ACL\PermissionController');
+        /**
+         * Routes Plans
+         */
+        Route::get('plans/create', 'PlanController@create')->name('plans.create');
+        Route::any('plans/search', 'PlanController@search')->name('plans.search');
+        Route::delete('plans/{id}', 'PlanController@delete')->name('plans.delete');
 
-    /**
-     * Route Home dashboard
-     */
-    Route::get('/', 'PlanController@index')->name('admin.index');
+        Route::post('plans', 'PlanController@store')->name('plans.store');
+        Route::get('plans', 'PlanController@index')->name('plans.index');
+
+        Route::get('plans/{url}', 'PlanController@show')->name('plans.show');
+        Route::put('plans/{url}', 'PlanController@update')->name('plans.update');
+        Route::get('plans/{url}/edit', 'PlanController@edit')->name('plans.edit');
+
+
+        /**
+         * Routes Profiles
+         */
+
+        Route::any('profiles/search', 'ACL\ProfileController@search')->name('profiles.search');
+        Route::resource('profiles', 'ACL\ProfileController');
+
+        /**
+         * Plan x Profile
+         */
+        Route::get('plans/{id}/profile/{idProfile}/detach', 'ACL\PlanProfileController@detachProfilePlan')->name('plans.profile.detach');
+        Route::post('plans/{id}/profiles', 'ACL\PlanProfileController@attachProfilesPlan')->name('plans.profiles.attach');
+        Route::any('plans/{id}/profiles/create', 'ACL\PlanProfileController@profilesAvailable')->name('plans.profiles.available');
+        Route::get('plans/{id}/profiles', 'ACL\PlanProfileController@profiles')->name('plans.profiles');
+        Route::get('profiles/{id}/plans', 'ACL\PlanProfileController@plans')->name('profiles.plans');
+        /**
+         * Routes Permissions x Profiles
+         */
+        Route::get('profiles/{id}/permission/{idPermission}/detach', 'ACL\PermissionProfileController@detachPermissionProfile')->name('profiles.permission.detach');
+        Route::any('profiles/{id}/permissions/create', 'ACL\PermissionProfileController@permissionsAvailable')->name('profiles.permissions.available');
+        Route::post('profiles/{id}/permissions', 'ACL\PermissionProfileController@attachPermissionsProfile')->name('profiles.permissions.attach');
+        Route::get('profiles/{id}/permissions', 'ACL\PermissionProfileController@permissions')->name('profiles.permissions');
+        Route::get('permission/{id}/profiles', 'ACL\PermissionProfileController@profiles')->name('permission.profiles');
+
+        /**
+         * Routes Permissions
+         */
+
+        Route::any('permissions/search', 'ACL\PermissionController@search')->name('permissions.search');
+        Route::resource('permissions', 'ACL\PermissionController');
+
+        /**
+         * Route Home dashboard
+         */
+        Route::get('/', 'PlanController@index')->name('admin.index');
+    });
+
+
+
+
+
+//Route::get('/', function () { return view('welcome'); });
+Route::get('/','App\Http\Controllers\Site\SiteController@index')->name('site.home');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/forgot-password', function () {
+    return view('auth.forgot-password');
+})->middleware('guest')->name('password.request');
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+require __DIR__ . '/auth.php';
