9f30f1fe273956ee363d9c385ff99d0a969ec196
Pedro O Germano <pogermano@gmail.com>
2023-03-13 15:14:30 -0300

    create model & tenant model

---
+++ app/Models/Tenant.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tenant extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'cnpj', 'name', 'url', 'email', 'logo', 'active',
+        'subscription', 'expires_at', 'subscription_id', 'subscription_active', 'subscription_suspended',
+    ];
+}
